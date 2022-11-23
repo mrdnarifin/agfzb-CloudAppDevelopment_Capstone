24c2204aebc943dbb76a30433016dee741d3b5d3
Dwi <mr.dnarifin@gmail.com>
2022-11-23 14:04:40 -0500

    Update REST

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf , get_dealer_by_id
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -90,14 +90,30 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        context = {}
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/52484a56-0e73-495c-bfbc-23c4b6b60500/dealership-package/get-dealership.json"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+
+        context['dealers'] = dealerships
+        # Return a list of dealer short name
+        return render(request, 'djangoapp/dealers.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+
+    if request.method == "GET":
+        context={}
+        # TODO update URL below
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/52484a56-0e73-495c-bfbc-23c4b6b60500/dealership-package/get-dealership.json"
+        dealer = get_dealer_by_id(url, id=dealer_id)
+        print(dealer)
+
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
